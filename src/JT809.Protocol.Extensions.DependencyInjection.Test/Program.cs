7b205a9e11717280ae9ff55ef363af62d493a518
SmallChi <564952747@qq.com>
2018-10-14 10:14:29 +0800

    1.修改头部版本号为公开可写 2.增加DI注入扩展方式

---
+++ src/JT809.Protocol.Extensions.DependencyInjection.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using JT809.Protocol.Extensions.DependencyInjection.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace JT809.Protocol.Extensions.DependencyInjection.Test
+{
+    class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            var serverHostBuilder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddJT809Configure(hostContext.Configuration.GetSection("JT809Options"));
+                });
+            await serverHostBuilder.RunConsoleAsync();
+        }
+    }
+}
